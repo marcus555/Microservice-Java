fd6c76680f86f9504512a640153944946b992cc3
Marcus Van Hellsing <marcos_advance555@hotmail.com>
2022-07-29 02:18:45 -0500

    Se actualiza repositorio UserAccount

--- src/main/java/com/marcus/usersmanagement/model/repository/UserAccountRepository.java
+++ src/main/java/com/marcus/usersmanagement/model/repository/UserAccountRepository.java
@@ -4,9 +4,7 @@ import com.marcus.usersmanagement.model.entity.UserAccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccountEntity, String> {
-    Optional<UserAccountEntity> findByUsername(String username);
+    UserAccountEntity findByUsername(String username);
 }
